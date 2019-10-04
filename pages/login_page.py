b7464fd91e13ee17402476ee2cf19a147db6ee54
kzholik <kris986@mail.ru>
2019-10-04 17:23:35 +0200

    tests after redirect to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,19 +10,26 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        url = self.browser.current_url()
-        assert url.find('/login/')
+        url = self.browser.current_url
+        assert url.find('/login/') != -1, 'Login URL is not correct'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_presents(*LoginPageLocators.LOGIN_FORM)
-        assert self.is_element_presents(*LoginPageLocators.LOGIN_INPUT_EMAIL)
-        assert self.is_element_presents(*LoginPageLocators.LOGIN_INPUT_PASSWORD)
+        assert self.is_element_presents(*LoginPageLocators.LOGIN_FORM), 'Login Form is not presented'
+        assert self.is_element_presents(*LoginPageLocators.LOGIN_INPUT_EMAIL), 'Email input for login  is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.LOGIN_INPUT_PASSWORD), 'Password input for login  is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.LOGIN_BUTTON_SUBMIT), 'Submit button for login  is not presented'
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_presents(*LoginPageLocators.LOGIN_FORM)
-        assert self.is_element_presents(*LoginPageLocators.REGISTRATION_INPUT_EMAIL)
-        assert self.is_element_presents(*LoginPageLocators.REGISTRATION_INPUT_PASSWORD)
-        assert self.is_element_presents(*LoginPageLocators.REGISTRATION_REPEAT_INPUT_PASSWORD)
-        assert self.is_element_presents(*LoginPageLocators.REGISTRATION_BUTTON_SUBMIT)
+        assert self.is_element_presents(*LoginPageLocators.REGISTRATION_FORM), 'Registration Form is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.REGISTRATION_INPUT_EMAIL), 'Email input for registration  is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.REGISTRATION_INPUT_PASSWORD), 'Password input for registration  is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.REGISTRATION_REPEAT_INPUT_PASSWORD), 'Reapeat password input for registration  is not presented'
+        assert self.is_element_presents(
+            *LoginPageLocators.REGISTRATION_BUTTON_SUBMIT), 'Submit button for registration  is not presented'
