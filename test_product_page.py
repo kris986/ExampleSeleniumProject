ba7056a8b8c3d396b7ed6f188995086674cdc308
admin <jenkins@example.com>
2019-10-08 01:13:28 +0200

    prepare code for review

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ class TestUserAddToBasketFromProductPage:
         self.registration_page.open()
         self.registration_page.register_new_user(email, password)
 
-    @pytest.mark.skip
+    @pytest.mark.need_review
     @pytest.mark.parametrize('link',
                              ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                               "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -53,6 +53,30 @@ class TestUserAddToBasketFromProductPage:
         product_page.should_be_not_scs_msg_adding_to_basket()
 
 
+@pytest.mark.need_review
+@pytest.mark.parametrize('link',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          pytest.param(
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                              marks=pytest.mark.xfail),
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, f'{link}')
+    product_page.open()
+    product_page.should_be_product_page()
+    product_page.add_product_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_msg_added_to_basket()
+    product_page.should_be_correct_amount_basket()
+
+
 def test_guest_cant_see_success_message(browser):
     product_page = ProductPage(browser, url_page)
     product_page.open()
@@ -75,6 +99,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     product_page = ProductPage(browser, url_page)
     product_page.open()
@@ -100,6 +125,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     product_page = ProductPage(browser, url_page)
     product_page.open()
